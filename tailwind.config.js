168354daed9a0303eab38d61af9a3861ad098423
gavinashley2 <gavinashley2@gmail.com>
2026-04-12 11:38:43 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0a2540',
+        gold: '#d4af37',
+      },
+    },
+  },
+  plugins: [],
+};
